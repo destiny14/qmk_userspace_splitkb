e7ffa15e0956024de3e4c1661c486c0c9cdeeb6b
Marco Stambor <mail@marco-stambor.de>
2026-06-25 22:55:20 +0200

    lets try this

--- keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/solaris_hlc/keymap.c
@@ -61,7 +61,7 @@ enum {
 #define SYM_HASH KC_BSLS // #
 #define SYM_DOLR LSFT(KC_4) // $
 #define SYM_PIPE RALT(KC_NONUS_BACKSLASH) // |
-#define SYM_MPIP LALT(KC_NONUS_BACKSLASH) // |
+#define SYM_MPIP LALT(KC_GRV) // |
 #define SYM_TLDE RALT(KC_RBRC) // ~
 #define SYM_BTCK LSFT(KC_EQL) // `
 #define SYM_EXCL LSFT(KC_1) // !
